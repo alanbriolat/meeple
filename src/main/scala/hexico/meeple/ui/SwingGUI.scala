1f0f6d9498f51d66d513c0020fed45fbc82cc3b4
Alan Briolat <alan.briolat@gmail.com>
2013-12-12 22:09:55 +0000

    Offload tile rendering to TileRenderer class

--- src/main/scala/hexico/meeple/ui/SwingGUI.scala
+++ src/main/scala/hexico/meeple/ui/SwingGUI.scala
@@ -1,97 +1,29 @@
 package hexico.meeple.ui
 
 import scala.swing._
-import java.awt.{BasicStroke, Color}
 import hexico.meeple.game._
-import hexico.meeple.game.Direction._
 import scala.util.Random
 
 object SwingGUI extends SimpleSwingApplication {
   val tileset = Tileset.base
   val random = new Random()
   val board: Board = new Board
-  for (x <- 0 to 3; y <- 0 to 3) {
+  for (x <- 0 to 6; y <- 0 to 6) {
     board.addTile(tileset.tiles(random.nextInt(tileset.tiles.length)), (x, y))
-  }
-
-  def directionToPoint(d: Direction): (Float, Float) = d match {
-    case NW => (-1f, -1f)
-    case N => (0f, -1f)
-    case NE => (1f, -1f)
-    case E => (1f, 0f)
-    case SE => (1f, 1f)
-    case S => (0f, 1f)
-    case SW => (-1f, 1f)
-    case W => (-1f, 0f)
   }
 
   class BoardPanel (val board: Board) extends Label {
     val TILE_SIZE: Int = 50
-    val COLOR_GRASS: Color = Color.GREEN
-    val COLOR_CITY: Color = Color.GRAY
-    val STROKE_CITY: BasicStroke = new BasicStroke(10, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)
-    val COLOR_ROAD: Color = Color.BLACK
-    val STROKE_ROAD: BasicStroke = new BasicStroke(1)
-    val COLOR_MONASTERY: Color = Color.RED
-
-    def location(t: (Int, Int), p: (Float, Float)): (Int, Int) = {
-      val (tx, ty) = t
-      val (px, py) = p
-      val scale: Int = TILE_SIZE / 2
-      ((tx * TILE_SIZE) + scale + (px * scale).toInt,
-       (ty * TILE_SIZE) + scale + (py * scale).toInt)
-    }
 
     override def paintComponent(g: Graphics2D) {
-      val (xmin, xmax, ymin, ymax) = board.extent
-      preferredSize = new Dimension((xmax - xmin + 1) * TILE_SIZE + 1,
-                                    (ymax - ymin + 1) * TILE_SIZE + 1)
+      val (minX, maxX, minY, maxY) = board.extent
+      preferredSize = new Dimension((maxX - minX + 1) * TILE_SIZE + 1,
+                                    (maxY - minY + 1) * TILE_SIZE + 1)
+
+      val renderer = new TileRenderer(TILE_SIZE, TILE_SIZE)
 
       for (((tx, ty), t) <- board.tiles) {
-        val loc = location((tx - xmin, ty - ymin), _: (Float, Float))
-        val (baseX, baseY) = loc((-1.0f, -1.0f))
-        val (centreX, centreY) = loc((0.0f, 0.0f))
-
-        g.setColor(COLOR_GRASS)
-        g.fillRect(baseX, baseY, TILE_SIZE, TILE_SIZE)
-
-        for (Feature(kind, points, contains) <- t.features) {
-          kind match {
-            case City =>
-              g.setColor(COLOR_CITY)
-              g.setStroke(STROKE_CITY)
-              val pxpoints = for {
-                d <- points.toVector.sorted
-                (relx, rely) = directionToPoint(d)
-                (x, y) = loc((relx * 0.8f, rely * 0.8f))
-              } yield (x, y)
-              val (xs, ys) = pxpoints.unzip
-              g.drawPolyline(xs.toArray, ys.toArray, xs.length)
-            case Road =>
-              g.setColor(COLOR_ROAD)
-              g.setStroke(STROKE_ROAD)
-              points.toList match {
-                case a::Nil =>
-                  val (relx, rely) = directionToPoint(a)
-                  val (startx, starty) = loc((relx, rely))
-                  val (endx, endy) = loc((relx * 0.3f, rely * 0.3f))
-                  g.drawLine(startx, starty, endx, endy)
-                  g.fillRect(endx - 2, endy - 2, 5, 5)
-                case a::b::Nil =>
-                  val (startx, starty) = loc(directionToPoint(a))
-                  val (endx, endy) = loc(directionToPoint(b))
-                  g.drawLine(startx, starty, endx, endy)
-                case _ =>
-                  println("impossible road")
-              }
-            case Monastery =>
-              g.setColor(COLOR_MONASTERY)
-              g.fillRect(centreX - 10, centreY - 10, 21, 21)
-            case _ => println("lulz")
-          }
-        }
-        //g.setColor(Color.WHITE)
-        //g.drawRect(baseX, baseY, TILE_SIZE, TILE_SIZE)
+        g.drawImage(renderer.render(t), null, tx * TILE_SIZE, ty * TILE_SIZE)
       }
     }
   }
---
+++ src/main/scala/hexico/meeple/ui/TileRenderer.scala
@@ -0,0 +1,72 @@
+package hexico.meeple.ui
+
+import java.awt.image.BufferedImage
+import hexico.meeple.game._
+import hexico.meeple.game.Direction._
+import java.awt.{BasicStroke, Color}
+
+
+class TileRenderer (val width: Int, val height: Int) {
+  val COLOR_GRASS: Color = Color.GREEN
+  val COLOR_CITY: Color = Color.GRAY
+  val STROKE_CITY: BasicStroke = new BasicStroke(10, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)
+  val COLOR_ROAD: Color = Color.BLACK
+  val STROKE_ROAD: BasicStroke = new BasicStroke(1)
+  val COLOR_MONASTERY: Color = Color.RED
+
+  def directionToPoint(d: Direction, scale: Double = 1.0): (Int, Int) = {
+    // Position of the edge relative to the (0.5, 0.5) centre of the tile
+    val (edgeX, edgeY) = d match {
+      case NW => (-0.5, -0.5)
+      case N  => (0.0, -0.5)
+      case NE => (0.5, -0.5)
+      case E  => (0.5, 0.0)
+      case SE => (0.5, 0.5)
+      case S  => (0.0, 0.5)
+      case SW => (-0.5, 0.5)
+      case W  => (-0.5, 0.0)
+    }
+    // Return pixel position within tile, applying scaling factor to distance from centre
+    fractionToPixel(0.5 + edgeX * scale, 0.5 + edgeY * scale)
+  }
+
+  def fractionToPixel(f: (Double, Double)): (Int, Int) = ((f._1 * width).toInt, (f._2 * height).toInt)
+
+  def render(t: Tile): BufferedImage = {
+    val i = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    val g = i.createGraphics()
+
+    g.setBackground(COLOR_GRASS)
+    g.clearRect(0, 0, width, height)
+
+    for (Feature(kind, points, contains) <- t.features) {
+      kind match {
+        case Road =>
+          g.setColor(COLOR_ROAD)
+          g.setStroke(STROKE_ROAD)
+          points.toList match {
+            case a::Nil =>
+              val (startX, startY) = directionToPoint(a)
+              val (endX, endY) = directionToPoint(a, 0.3)
+              g.drawLine(startX, startY, endX, endY)
+              g.fillRect(endX - 2, endY - 2, 5, 5)
+            case a::b::Nil =>
+              val (startX, startY) = directionToPoint(a)
+              val (endX, endY) = directionToPoint(b)
+              g.drawLine(startX, startY, endX, endY)
+            case _ =>
+              println("impossible road")
+          }
+        case Monastery =>
+          g.setColor(COLOR_MONASTERY)
+          val (startX, startY) = fractionToPixel(0.3, 0.3)
+          val (spanX, spanY) = fractionToPixel(0.4, 0.4)
+          g.fillRect(startX, startY, spanX, spanY)
+        case _ =>
+      }
+    }
+
+    // Return BufferedImage
+    i
+  }
+}
